c96fdb00aba6aaf5833bdd2bbd852d88433edea1
Izamar94 <cnm_cruz@hotmail.com>
2020-06-17 13:24:59 -0500

    servicios

--- components/futbol.php
+++ components/futbol.php
@@ -1,7 +1,7 @@
 <div class="container">
       <div class="py-2">
 
-        <div class="card light-blue accent-1  text-white">
+        <div class="card cardStyles">
         <div class="card-header  ">
             <h2>Equipo de futbol</h2>
         </div>
