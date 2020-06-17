4f01987432746f1cba9f2004b611f91af4cae8b9
Izamar94 <cnm_cruz@hotmail.com>
2020-06-16 21:08:28 -0500

    cambios

--- components/instalaciones.php
+++ components/instalaciones.php
@@ -3,11 +3,11 @@
 
         <div class="card light-blue accent-1  text-white">
         <div class="card-header  ">
-            <h2>Nuestras Instalaciones</h2>
+            <h2 class="text-dark">Nuestras Instalaciones</h2>
         </div>
         <div class="card-body ">
             <blockquote class="blockquote mb-0">
-            <p>En el Centro Universitario Hidalguense, contamos con instalaciones ex profeso, acondicionadas para favorecer un ambiente de estudio y trabajo confortable.
+            <p class="text-dark text-justify">En el Centro Universitario Hidalguense, contamos con instalaciones ex profeso, acondicionadas para favorecer un ambiente de estudio y trabajo confortable.
                 Nos esforzamos por mantenerlas permanentemente limpias, seguras, cómodas y agradables, te invitamos a conocer nuestras distintas áreas: Aulas de clase, sala de usos múltiples, sala de juicios orales, camara de gesell, laboratorios de cómputo, y más.</p>
           </blockquote>
            <div>
--- components/nosotros.php
+++ components/nosotros.php
@@ -3,11 +3,11 @@
 
         <div class="card light-blue accent-1 text-white">
         <div class="card-header  ">
-            <h2>Misión </h2>
+            <h2 class="text-dark">Misión </h2>
         </div>
         <div class="card-body ">
             <blockquote class="blockquote mb-0">
-            <p>Formar mujeres y hombres libres, humanistas de pensamiento creativo y crítico, con una sólida preparación científica, técnica y cultural, desarrollando en ellos, los valores universales y excelencia profesional; lo cual les permitirá alta competitividad profesional para una mejor convivencia y eficaz transformación de la sociedad.</p>
+            <p class="text-justify text-dark">Formar mujeres y hombres libres, humanistas de pensamiento creativo y crítico, con una sólida preparación científica, técnica y cultural, desarrollando en ellos, los valores universales y excelencia profesional; lo cual les permitirá alta competitividad profesional para una mejor convivencia y eficaz transformación de la sociedad.</p>
           
             </blockquote>
         </div>
@@ -15,15 +15,13 @@
 
     </div>
     <div class="py-2">
-
        <div class="card light-blue accent-1 text-white">
         <div class="card-header ">
-            <h2>Visión </h2>
+            <h2 class="text-dark">Visión </h2>
         </div>
         <div class="card-body ">
             <blockquote class="blockquote mb-0">
-            <p>Ser una institución de enseñanza superior, cuya visión integral e innovadora de la educación; promueva y sustente total calidad académica, sólida estructura ética y profesionalismo. Principios que distinguen a la institución y forja en sus estudiantes, a fin de cumplir sus metas con éxito laboral, profesional y cultural en su entorno social.</p>
-            
+            <p class="text-justify text-dark">Ser una institución de enseñanza superior, cuya visión integral e innovadora de la educación; promueva y sustente total calidad académica, sólida estructura ética y profesionalismo. Principios que distinguen a la institución y forja en sus estudiantes, a fin de cumplir sus metas con éxito laboral, profesional y cultural en su entorno social.</p>
             </blockquote>
         </div>
         </div>
@@ -33,11 +31,11 @@
 
       <div class="card light-blue accent-1 text-white">
         <div class="card-header ">
-           <h2> Filosofía Institucional</h2>
+           <h2 class="text-dark"> Filosofía Institucional</h2>
         </div>
         <div class="card-body ">
             <blockquote class="blockquote mb-0">
-            <p>Promover los valores universales del ser humano para favorecer un desarrollo integral y equilibrado del estudiante, futuro profesional. En la acción educativa del Centro Universitario Hidalguense, subyacen como principios fundamentales el respeto, el aprecio y la confianza en el ser humano, así como la búsqueda de la verdad.</p>
+            <p class="text-justify text-dark">Promover los valores universales del ser humano para favorecer un desarrollo integral y equilibrado del estudiante, futuro profesional. En la acción educativa del Centro Universitario Hidalguense, subyacen como principios fundamentales el respeto, el aprecio y la confianza en el ser humano, así como la búsqueda de la verdad.</p>
            
             </blockquote>
         </div>
