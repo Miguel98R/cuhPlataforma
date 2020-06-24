2d849f38a1cfcc8e3a4b156e531e3c10b5261565
Izamar94 <cnm_cruz@hotmail.com>
2020-06-24 13:40:49 -0500

    carrousel sistemas

--- eventos.php
+++ eventos.php
@@ -60,20 +60,18 @@
                       </div>
                     <!--/.Slides-->
                   <!--Controls-->
-  <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-  <!--/.Controls-->
-</div>
-<!--/.Carousel Wrapper-->
- 
-</section>
-     
+                    <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  <!--/.Controls-->
+                </div>
+              <!--/.Carousel Wrapper--> 
+            </section>   
 </div>
 
 </div>
