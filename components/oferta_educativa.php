3c710f130655ff2ef4dc4e761d83132eab997f96
Izamar94 <cnm_cruz@hotmail.com>
2020-06-24 20:56:52 -0500

    changes cards

--- components/oferta_educativa.php
+++ components/oferta_educativa.php
@@ -1,11 +1,11 @@
 <div class="container-fluid py-2">
-  <h3 class="article__title white-dark">OFERTA EDUCATIVA</h3>
+  <strong><h3 class="article__title white-dark">OFERTA EDUCATIVA</h3></strong>
   <div class="row">
     <div class="col-md-4 py-2">
       <!-- Card -->
-        <div class="card card-image" style="background-image: url('./img/LOGO-PREPA.png');">
+        <div class="card cardStyles">
           <!-- Content -->
-          <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
+          <div class=" text-center d-flex align-items-center py-5 px-4">
             <div>
               <h3 class="card-title pt-2" style="color: #304ffe"><strong>Preparatoria</strong></h3>
               <p>Atrévete a conocer nuestro sistema de estudios.</p>
@@ -17,9 +17,9 @@
     </div>
     <div class="col-md-4 py-2">
       <!-- Card -->
-        <div class="card card-image" style="background-image: url('./img/LOGO-CUH.png');">
+        <div class="card cardStyles" >
           <!-- Content -->
-          <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
+          <div class=" text-center d-flex align-items-center py-5 px-4">
             <div>
               <h3 class="card-title pt-2" style="color: #304ffe"><strong>Licenciatura</strong></h3>
               <p>Te ofrecemos la carrera que necesitas</p>
@@ -31,9 +31,9 @@
     </div>      
     <div class="col-md-4 py-2">
       <!-- Card -->
-        <div class="card card-image" style="background-image: url('./img/LOGO-POSGRADO.png');">
+        <div class="card cardStyles" >
           <!-- Content -->
-          <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
+          <div class=" text-center d-flex align-items-center py-5 px-4">
             <div>
               <h3 class="card-title pt-2" style="color: #304ffe"><strong>Maestría</strong></h3>
               <p>Cursa tu maestría en un año 4 meses.</p>
--- prepa.php
+++ prepa.php
@@ -2,19 +2,12 @@
 <body>
   <?php include "components/nav.php";?>
   <!-- Start your project here-->  
-  
 <?php include "components/header.php";?>
-
 <div class="container">
-
-
   <div class="menuAboutUs text-center" style="box-shadow: 1px 2px 16px -2px rgba(0,0,0,0.75);">
-    
-
     <section class="py-2 px-2">
  <!-- Card -->
 <div class="">
-
   <!-- Card image -->
   <div class="view overlay">
     <img class="card-img-top" src="img/prepa3.jpg"
@@ -25,50 +18,32 @@
       </div>
     </a>
   </div>
-
   <!-- Card content -->
-
   <div class="container-fluid py-2">
        <div class="row">
       <div class="col-md-8">
           <div class="jumbotron bg-uno text-white jumbotron-fluid">
   <div class="container">
     <h2 class="display-4">Objetivos Generales</h2>
-
     <p class="lead">Ofrecer al estudiante una formación metodológica e integral que lo capacite para el estudio de una licenciatura adquiriendo los conocimientos científicos, valores éticos, morales, cívicos y culturales que le permita desarrollar una actitud emprendedora para crear y transformar su entorno.</p>
   </div>
 </div>
       </div>
       <div class="col-md-4">
-            <div class="container">
-           
+            <div class="container">          
                     <div class="alert alert-primary" role="alert">
                    <p> <strong class="font-weight-bold">Tipo de Ciclo:</strong> Mensual | Bimestral</p>
-                        <p><strong  class="font-weight-bold">Termina tu prepa en:</strong> 1 Año 10 Meses</p>
-                  
+                        <p><strong  class="font-weight-bold">Termina tu prepa en:</strong> 1 Año 10 Meses</p>                 
                     </div>
                 </div>
       </div>
-
   </div>
-
   </div>
- 
-
   <div class="card-body ">
-
-    
-    
-
-      
-
-
         <!-- Card deck -->
 <div class="card-deck">
-
   <!-- Card -->
   <div class="card mb-4">
-
     <!--Card image-->
     <div class="view overlay">
       <img class="card-img-top" src="img/prepa1.jpg"
@@ -77,26 +52,19 @@
         <div class="mask rgba-white-slight"></div>
       </a>
     </div>
-
     <!--Card content-->
     <div class="card-body bg-uno text-white">
-
       <!--Title-->
       <h4 class="card-title">Te ofrecemos:</h4>
       <!--Text-->
       <p class="">Un plan de estudios dinámico, interdisciplinario, intensivo, presencial, activo y participativo con un sistema mensual escolarizado, con grupos reducidos, profesorado eficaz y altamente capacitado, horarios flexibles, instalaciones adecuadas, permitiendo también a las personas trabajadoras continuar con sus estudios de nivel medio superior.
-
 Nuestro plan de estudios está estructurado con 38 asignaturas distribuidas en 6 módulos que proporcionan conocimientos actuales necesarios para responder satisfactoriamente a los retos y demandas de estudios superiores y sociedad en conjunto.</p>
-      <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
-      
+      <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->     
     </div>
-
   </div>
   <!-- Card -->
-
   <!-- Card -->
   <div class="card mb-4">
-
     <!--Card image-->
     <div class="view overlay">
         <img class="card-img-top" src="img/prepa4.jpg"
@@ -105,10 +73,8 @@ Nuestro plan de estudios está estructurado con 38 asignaturas distribuidas en 6
         <div class="mask rgba-white-slight"></div>
       </a>
     </div>
-
     <!--Card content-->
     <div class="card-body bg-uno text-white">
-
       <!--Title-->
       <h4 class="card-title">Perfil del Aspirante</h4>
       <!--Text-->
@@ -120,26 +86,12 @@ Nuestro plan de estudios está estructurado con 38 asignaturas distribuidas en 6
                <li>Valores tales como: responsabilidad, ética, honestidad y respeto.</li>
                 <li>Habilidad de razonamiento.</li>
                  <li>Deseo de superación.</p></li>
-        </ul>  
-        
-        
-        
-
-
-
-
-
-
-
-      
+        </ul>   
     </div>
-
   </div>
   <!-- Card -->
-
   <!-- Card -->
   <div class="card mb-4">
-
     <!--Card image-->
     <div class="view overlay">
         <img class="card-img-top" src="img/prepa2.jpg"
@@ -148,10 +100,8 @@ Nuestro plan de estudios está estructurado con 38 asignaturas distribuidas en 6
         <div class="mask rgba-white-slight"></div>
       </a>
     </div>
-
     <!--Card content-->
      <div class="card-body bg-uno text-white">
-
       <!--Title-->
       <h4 class="card-title">Perfil de Egresado</h4>
       <!--Text-->
