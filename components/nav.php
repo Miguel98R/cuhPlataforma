ee4690793e33b111f180890ba7da01dedd4db16a
Izamar94 <cnm_cruz@hotmail.com>
2020-06-25 08:50:53 -0500

    menu hamburguesa

--- components/nav.php
+++ components/nav.php
@@ -34,17 +34,24 @@
     </ul>      -->
   <!-- </div> -->
 <!-- </nav> -->
- 
- <div class="sidebar justify-content-md-center justify-content-start" >
-  <a href="index.php" ><i class="fa fa-fw fa-home"></i> Inicio</a>
-  <a href="sobre_nosotros.php" ><i class="fa fa-fw fa-users"></i> Nosotros</a>
-  <a href="./plataforma/index.php" target=_blank ><i class="fa fa-fw fa-laptop"></i> Plataforma</a>
-  <a href="exalumnos.php" ><i class="fas fa-fw fa-user-graduate"></i> Ex-Alumnos</a>
-  <a href="prepa.php" ><i class="fa fa-fw fa-building"></i> Preparatoria</a>
-  <a href="licenciatura.php" ><i class="fa fa-fw fa-user-tie"></i> Licenciatura</a>
-  <a href="posgrado.php" ><i class="fa fa-fw fa-graduation-cap"></i> Posgrado</a>
-  <a href="logros.php" ><i class="fa fa-fw fa-trophy"></i> Logros Academicos</a>
-  <a href="eventos.php" ><i class="fa fa-fw fa-calendar-alt"></i> Eventos</a>
-</div>
+<nav class="navbarPrin navbar-dark navbar-expand-md bg-transparent justify-content-md-center ">    
+  <button class="navbar-toggler ml-1" type="button" data-toggle="collapse" data-target="#collapsingNavbar2">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="navbar-collapse collapse justify-content-between align-items-center w-100" id="collapsingNavbar2" >
+    <ul class="nav flex-column mx-auto ">
+      <a href="index.php" ><i class="fa fa-fw fa-home"></i> Inicio</a>
+      <a href="sobre_nosotros.php" ><i class="fa fa-fw fa-users"></i> Nosotros</a>
+      <a href="./plataforma/index.php" target=_blank ><i class="fa fa-fw fa-laptop"></i> Plataforma</a>
+      <a href="exalumnos.php" ><i class="fas fa-fw fa-user-graduate"></i> Ex-Alumnos</a>
+      <a href="prepa.php" ><i class="fa fa-fw fa-building"></i> Preparatoria</a>
+      <a href="licenciatura.php" ><i class="fa fa-fw fa-user-tie"></i> Licenciatura</a>
+      <a href="posgrado.php" ><i class="fa fa-fw fa-graduation-cap"></i> Posgrado</a>
+      <a href="logros.php" ><i class="fa fa-fw fa-trophy"></i> Logros Academicos</a>
+      <a href="eventos.php" ><i class="fa fa-fw fa-calendar-alt"></i> Eventos</a>
+    </ul>
+  </div>  
+ </nav>
+
 
 
