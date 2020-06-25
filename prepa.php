0a52dc1d370fa3d5e9d66571a85c0ddfb147bdea
Oswaldo <oswaldocuh@gmail.com>
2020-06-25 10:30:32 -0500

    Cards Prepa Horizontal

--- prepa.php
+++ prepa.php
@@ -39,89 +39,83 @@
       </div>
   </div>
   </div>
+  
+  <div class="card mb-3 cardStyles" style="max-width: auto;">
+  <div class="row no-gutters">
+    <div class="col-md-4 view overlay">
+      <img class="card-img-top" src="img/prepa1.jpg" alt="Card image cap">
+      <a href="#!">
+        <div class="mask rgba-white-slight"></div>
+      </a>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body ">
+        <h4 class="card-title">Te ofrecemos:</h4>
+        <p class="text-justify">Un plan de estudios dinámico, interdisciplinario, intensivo, presencial, activo y participativo con un sistema mensual escolarizado, con grupos reducidos, profesorado eficaz y altamente capacitado, horarios flexibles, instalaciones adecuadas, permitiendo también a las personas trabajadoras continuar con sus estudios de nivel medio superior.
+        Nuestro plan de estudios está estructurado con 38 asignaturas distribuidas en 6 módulos que proporcionan conocimientos actuales necesarios para responder satisfactoriamente a los retos y demandas de estudios superiores y sociedad en conjunto.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3 cardStyles" style="max-width: auto;">
+  <div class="row no-gutters">
+    <div class="col-md-4 view overlay">
+      <img class="card-img-top" src="img/prepa4.jpg" alt="Card image cap">
+      <a href="#!">
+        <div class="mask rgba-white-slight"></div>
+      </a>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body ">
+        <h4 class="card-title">Perfil del Aspirante</h4>
+        <p class="text-justify ">
+          <ul class="text-left ">
+            <li>Convicción para iniciar, continuar o concluir sus estudios de nivel medio superior.</li>
+            <li>Capacidad de trabajo en equipo.</li>
+            <li>iniciativa, creatividad, interés por la investigación.</li>
+            <li>Valores tales como: responsabilidad, ética, honestidad y respeto.</li>
+            <li>Habilidad de razonamiento.</li>
+            <li>Deseo de superación.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3 cardStyles" style="max-width: auto;">
+  <div class="row no-gutters">
+    <div class="col-md-4 view overlay">
+      <img class="card-img-top" src="img/prepa2.jpg" alt="Card image cap">
+      <a href="#!">
+        <div class="mask rgba-white-slight"></div>
+      </a>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body ">
+        <h4 class="card-title">Perfil de Egresado</h4>
+        <p class="text-justify ">
+        Al término del bachillerato el egresado será capaz de:
+        *Obtener los conocimientos, métodos y técnicas necesarias para incorporarse al mundo laboral o continuar su formación profesional a nivel superior.
+
+        *Desarrollar un pensamiento crítico y analítico.
+
+        *Tener una información integral, social y humanística que le permita comprender su contexto social y cultural.
+
+        *Desarrollar las habilidades necesarias para el autoaprendizaje.
+
+        *Actuar como agentes de cambio a través de toma de decisiones para la solución de problemas.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
   <div class="card-body ">
         <!-- Card deck -->
 <div class="card-deck">
-  <!-- Card -->
-  <div class="card mb-4">
-    <!--Card image-->
-    <div class="view overlay">
-      <img class="card-img-top" src="img/prepa1.jpg"
-        alt="Card image cap">
-      <a href="#!">
-        <div class="mask rgba-white-slight"></div>
-      </a>
-    </div>
-    <!--Card content-->
-    <div class="card-body cardStyles">
-      <!--Title-->
-      <h4 class="card-title">Te ofrecemos:</h4>
-      <!--Text-->
-      <p class="text-justify">Un plan de estudios dinámico, interdisciplinario, intensivo, presencial, activo y participativo con un sistema mensual escolarizado, con grupos reducidos, profesorado eficaz y altamente capacitado, horarios flexibles, instalaciones adecuadas, permitiendo también a las personas trabajadoras continuar con sus estudios de nivel medio superior.
-Nuestro plan de estudios está estructurado con 38 asignaturas distribuidas en 6 módulos que proporcionan conocimientos actuales necesarios para responder satisfactoriamente a los retos y demandas de estudios superiores y sociedad en conjunto.</p>
-      <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->     
-    </div>
-  </div>
-  <!-- Card -->
-  <!-- Card -->
-  <div class="card mb-4">
-    <!--Card image-->
-    <div class="view overlay">
-        <img class="card-img-top" src="img/prepa4.jpg"
-        alt="Card image cap">
-      <a href="#!">
-        <div class="mask rgba-white-slight"></div>
-      </a>
-    </div>
-    <!--Card content-->
-    <div class="card-body cardStyles">
-      <!--Title-->
-      <h4 class="card-title">Perfil del Aspirante</h4>
-      <!--Text-->
-      <p class="text-justify ">
-        <ul class="text-left ">
-            <li>Convicción para iniciar, continuar o concluir sus estudios de nivel medio superior.</li>
-             <li>Capacidad de trabajo en equipo.</li>
-              <li>iniciativa, creatividad, interés por la investigación.</li>
-               <li>Valores tales como: responsabilidad, ética, honestidad y respeto.</li>
-                <li>Habilidad de razonamiento.</li>
-                 <li>Deseo de superación.</p></li>
-        </ul>   
-    </div>
-  </div>
-  <!-- Card -->
-  <!-- Card -->
-  <div class="card mb-4">
-    <!--Card image-->
-    <div class="view overlay">
-        <img class="card-img-top" src="img/prepa2.jpg"
-        alt="Card image cap">
-      <a href="#!">
-        <div class="mask rgba-white-slight"></div>
-      </a>
-    </div>
-    <!--Card content-->
-     <div class="card-body cardStyles">
-      <!--Title-->
-      <strong><h4 class="card-title">Perfil de Egresado</h4></strong>
-      <!--Text-->
-      <p class="text-justify">Al término del bachillerato el egresado será capaz de:
-*Obtener los conocimientos, métodos y técnicas necesarias para incorporarse al mundo laboral o continuar su formación profesional a nivel superior.
-
-*Desarrollar un pensamiento crítico y analítico.
-
-*Tener una información integral, social y humanística que le permita comprender su contexto social y cultural.
-
-*Desarrollar las habilidades necesarias para el autoaprendizaje.
-
-*Actuar como agentes de cambio a través de toma de decisiones para la solución de problemas.
-
-.</p>
-   
-    </div>
-
-  </div>
-  <!-- Card -->
+  
+  
 
 </div>
 <!-- Card deck -->
